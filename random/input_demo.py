5284f61e0f6d1053657e112d1441c0d3f882fc35
Jameel Bokhari (macbook) <jameelbokhari@gmail.com>
2018-09-05 21:46:19 -0700

    Initial commit

---
+++ random/input_demo.py
@@ -0,0 +1,27 @@
+names =  input('Enter names separated by commas:').split(',')
+assignments =  input('Enter assignment counts separated by commas:').split(',')
+grades =  input('Enter grades separated by commas (0-100):').split(',')
+
+if len(names) != len(assignments) or len(assignments) != len(grades):
+	print("Error: Lists were not equal length")
+	exit()
+
+# message string to be used for each student
+# HINT: use .format() with this string in your for loop
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+print(names,assignments,grades)
+
+def get_potential_grade(assignment_count, grade):
+	potential_grade = int(assignment_count) * 2 + float(grade)
+	return potential_grade
+
+for i, name in enumerate(names):
+	grade = str(grades[i])
+	assignment_count = assignments[i]
+	potential_grade = get_potential_grade( assignment_count, grade )
+	potential_grade = str(potential_grade)
+	final_message = message.format(name, assignment_count, grade, potential_grade )
+	print( final_message )
